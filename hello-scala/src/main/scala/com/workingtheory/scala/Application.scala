053dc7a0940504b1deee8b6ecb4d82a581b13257
Kedar Joshi <kedar.joshi@locationguru.com>
2017-01-01 15:44:51 +0530

    Added scala module

---
+++ hello-scala/src/main/scala/com/workingtheory/scala/Application.scala
@@ -0,0 +1,13 @@
+package com.workingtheory.scala
+
+import org.apache.logging.log4j.{LogManager, Logger}
+
+object Application
+{
+	private val logger: Logger = LogManager.getLogger(Application.this)
+
+	def main(args: Array[String]): Unit =
+	{
+
+	}
+}
